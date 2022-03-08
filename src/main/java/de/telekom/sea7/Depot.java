8236de6b7f7fb020ae8c3e0bf00a1189301b1a48
Robert Esser <robert-esser_student2021@wilder-school>
2022-03-08 15:21:45 +0100

    Version2

--- src/main/java/de/telekom/sea7/Depot.java
+++ src/main/java/de/telekom/sea7/Depot.java
@@ -3,7 +3,7 @@ package de.telekom.sea7;
 public class Depot {
 
 	private EntityIBAN[] depotArray = new EntityIBAN[9];
-		
+
 	private int size;
 
 	public Depot() {
@@ -16,8 +16,26 @@ public class Depot {
 	}
 
 	// Gibt das Objekt von der angegebenen Stelle (index) zurück
-	public EntityIBAN get(int index) {
-		return depotArray[index];
+//	public EntityIBAN get(int index) {
+//		return depotArray[index];
+//	}
+	public EntityIBAN get(int index) throws OwnException {
+		if (index > size) {
+			throw new OwnException("Index muss kleiner Size sein");
+		}
+		if (index < 0) {
+			throw new OwnException();
+		}
+//		if (depotArray[index].getIban() == null) {
+//			throw new OwnException();
+//		}
+		if (index == 0 && size == 0) {
+			throw new OwnException();
+		}
+
+		else {
+			return depotArray[index];
+		}
 	}
 
 	// Gibt die Anzahl der belegten Stellen des Arrays zurück
--- src/test/java/de/telekom/sea7/DepotTest.java
+++ src/test/java/de/telekom/sea7/DepotTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
@@ -21,60 +22,99 @@ public class DepotTest {
 		cut = new Depot();
 	}
 
+//***************Vergleich Grösse Array nach ADD************
 	@Test
-	void Depot_add_test() {
+	void Depot_add_test1() {
 		var result = cut.size();
 		cut.add(new EntityIBAN(1, "DE12345"));
 		var result2 = cut.size();
 		assertNotEquals(result, result2);
 	}
 
+	// ***************Vergleich Inhalt nach ADD************
 	@Test
-	void Depot_add_test2() {
+	void Depot_add_test2() throws OwnException {
 		cut.add(new EntityIBAN(1, "DE12345"));
 		assertSame(cut.get(0).getIban(), "DE12345");
 	}
 
+	// ***************Prüfung ob leer nach ADD************
 	@Test
-	void Depot_add_test3() {
+	void Depot_add_test3() throws OwnException {
 		cut.add(new EntityIBAN(1, ""));
-		assertFalse(cut.get(0).getIban().isEmpty());
+		assertTrue(cut.get(0).getIban().isEmpty());
 	}
 
+	// ***************Prüfung auf Exception nach zu grossem ADD************
 	@Test
 	void Depot_add_test5() {
-		boolean thrown = false;
+		boolean thrownyn = false;
+		cut.add(new EntityIBAN(1, "DE12345"));
+		cut.add(new EntityIBAN(2, "DE12345"));
+		cut.add(new EntityIBAN(3, "DE12345"));
+		cut.add(new EntityIBAN(4, "DE12345"));
+		cut.add(new EntityIBAN(5, "DE12345"));
+		cut.add(new EntityIBAN(6, "DE12345"));
+		cut.add(new EntityIBAN(7, "DE12345"));
+		cut.add(new EntityIBAN(8, "DE12345"));
+		cut.add(new EntityIBAN(9, "DE12345"));
 		try {
-			cut.add(new EntityIBAN(1, "DE12345"));
-			cut.add(new EntityIBAN(2, "DE12345"));
-			cut.add(new EntityIBAN(3, "DE12345"));
-			cut.add(new EntityIBAN(4, "DE12345"));
-			cut.add(new EntityIBAN(5, "DE12345"));
-			cut.add(new EntityIBAN(6, "DE12345"));
-			cut.add(new EntityIBAN(7, "DE12345"));
-			cut.add(new EntityIBAN(8, "DE12345"));
-			cut.add(new EntityIBAN(9, "DE12345"));
+
 			cut.add(new EntityIBAN(10, "DE12345"));
-			cut.add(new EntityIBAN(11, "DE12345"));
+		
 		} catch (IndexOutOfBoundsException e) {
-			thrown = true;
+			thrownyn = true;
 		}
-		assertTrue(thrown);
+		assertTrue(thrownyn);
 	}
 
+	// ***************Prüfung Array-Groesse nach ADD************
 	@Test
-	void sizeTest() {
+	void sizeTest1() {
 		cut.add(new EntityIBAN(1, "DE12345"));
 		cut.add(new EntityIBAN(2, "DE12346"));
 		var result = cut.size();
 		assertTrue(result == 2);
 	}
 
+	// ***************Prüfung Array-Inhalt mit GET************
 	@Test
-	void Depot_get_test() {
+	void Depot_get_test1() throws OwnException {
 		cut.add(new EntityIBAN(1, "DE12345"));
 		cut.add(new EntityIBAN(2, "DE12346"));
 		assertEquals(cut.get(1).getIban(), "DE12346");
+	}
+
+	@Test
+	void Depot_get_test2() {
+		boolean thrownyn = false;
+		try {
+			cut.add(new EntityIBAN(1, "DE12345"));
+			cut.add(new EntityIBAN(2, "DE12346"));
+			cut.get(-1);
+		} catch (OwnException e) {
+			thrownyn = true;
+		}
+		assertTrue(thrownyn);
+	}
+
+	// ***************Prüfung mit Lambda-Code Array-Inhalt mit GET(out of
+	// Range)************
+	@Test
+	void Depot_get_test3() {
+		cut.add(new EntityIBAN(1, "DE12345"));
+		cut.add(new EntityIBAN(2, "DE12346"));
+		assertThrows(OwnException.class, () -> {
+			cut.get(3);
+		});
+	}
+
+	// ***************Prüfung mit Lambda-Code Array-Inhalt mit GET(-1)************
+	@Test
+	void Depot_get_test4() {
+		assertThrows(OwnException.class, () -> 
+			cut.get(-1)
+		);
 	}
 
 	@AfterEach
